1d3455b199f06604978c53cf2f6d5bc2b2c91abe
hishamls <modernhisham@gmail.com>
2023-09-12 23:32:02 +0300

    Using CSS Module

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -1,7 +1,54 @@
 import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
-import "./TaskInput.css";
+import classes from "./TaskInput.module.css"; // MAKE ALSO UNIQUE CLASSES
+// import styled from "styled-components"; // YOU HAVE TO INSTALL ITS PACKAGE FIRSTLY.
+// WE USE IT TO MAKE EVERY CLASS UNIQUE
+
+// const FormControl = styled.div`
+//   // IT A METHOD LIKE STYLED.DIV() = STYLED.DIV``\SO THEY HAVE PROPS
+//   .form-control {
+//     margin: 0.5rem 0;
+//   }
+
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${(props) => (props.onInvalid ? "red" : "black")};
+//   }
+
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.onInvalid ? "red" : "gray")};
+//     background-color: ${(props) => (props.onInvalid ? "pink" : "transparent")};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+
+//   .form-control {
+//     display: flex;
+//     flex-direction: column;
+//     gap: 1rem;
+//   }
+
+//   /* &.invalid input {
+//     border-color: red;
+//     background-color: pink;
+//   } */
+//   /* &.invalid label {
+//     font-weight: bold;
+//     color: red;
+//   } */
+// `;
 
 const TaskInput = (props) => {
   const [nText, setNText] = useState("");
@@ -32,7 +79,14 @@ const TaskInput = (props) => {
   return (
     <form onSubmit={submitHandler}>
       {/* NOT WORK <div className={`form-control ${isValid ? "" : "invalid"}`}> */}
-      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+      {/* <FormControl className={`form-control ${!isValid ? "invalid" : ""}`}> */}
+      {/* <FormControl onInvalid={!isValid}> */}
+      <div
+        className={`${classes["form-control"]} ${
+          // !isValid ? classes.invalid : "" // ==
+          !isValid && classes.invalid
+        }`}
+      >
         <label
         // style={{ color: isValid ? "black" : "red" }}
         >
